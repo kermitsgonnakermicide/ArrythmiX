0a3aed1d131a5e01969201a26dc123d60c0ffd04
Ishaan Adhikari <adh.ishaan10@gmail.com>
2025-11-07 09:45:16 +0530

    sleep instead of busy waiting for better power consumption, need to transmit IMU

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 #include <Arduino.h>
 #include "LSM6DS3.h"
 #define SAMPLE_PERIOD 2778
+#define TIMER_INTERRUPT_DEBUG         0
+#define _TIMERINTERRUPT_LOGLEVEL_     0
+#include <NRF52TimerInterrupt.h>
+#include <NRF52_ISR_Timer.h>
+
 #define ECG_PIN    A0    // Analog ECG output
 #define LO_PLUS    D1    // Lead-off detection + //try changing to d2
 #define LO_MINUS   D2    // Lead-off detection -
 #define BATTERY_PIN A5
 bool low_battery = false;
-LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
-
+LSM6DS3 myIMU(I2C_MODE, 0x6A);
+NRF52Timer ITimer(NRF_TIMER_1);
+NRF52_ISR_Timer ISR_Timer;
 void blinkLed(int color, int no) {
         digitalWrite(color, HIGH);
         delay(200);
@@ -37,24 +43,34 @@ class BLEStatus : public NimBLEServerCallbacks {
 public:
     bool connected = false;
     void onConnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo) {
-        Serial.println("Device Connected");
         blinkLed(LED_GREEN,2);
         connected = true;
     }
     void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason) {
-        Serial.printf("Device Disconnected %d\n", reason);
         beginAdvertising();
         connected = false;
     }
 };
-
+void sendECGData() {
+    String Value;
+    int loPlusState = digitalRead(LO_PLUS);
+    int loMinusState = digitalRead(LO_MINUS);
+    unsigned long ecgValue = analogRead(ECG_PIN);
+    if (loPlusState == HIGH || loMinusState == HIGH) {
+        Value = "Leads Off";
+        blinkLed(LED_RED,2);
+    } else {
+        Value = String(ecgValue);
+    }
+    characteristic->setValue(Value.c_str());
+    characteristic->notify();
+}
 BLEStatus bleStatus;
 void setup() {
     delay(5000);
-    Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     blinkLed(LED_BLUE,2);
-
+    ITimer.attachInterrupt(360.0, sendECGData);
     NimBLEDevice::init("ECG Data");
     NimBLEDevice::setOwnAddrType(BLE_OWN_ADDR_RANDOM);
     NimBLEServer* pServer = NimBLEDevice::createServer();
@@ -85,60 +101,11 @@ void setup() {
     pinMode(LO_PLUS, INPUT_PULLUP);
     pinMode(LO_MINUS, INPUT_PULLUP);
     beginAdvertising();
-    Serial.println("NimBLE running");
     blinkLed(LED_GREEN,3);
-    Serial.println(NimBLEDevice::getAddress().toString().c_str());
-    if (myIMU.begin() != 0) {
-        Serial.println("Device error");
-    } else {
-        Serial.println("Device OK!");
-    }
-}
 
-int i = 0;
-unsigned long timeSinceLastBlink = 0;
-bool builtinLed_Status = false;
+}
 void loop() {
-    // Serial.print("\nAccelerometer:\n");
-    // Serial.print(" X1 = ");
-    // Serial.println(myIMU.readFloatAccelX(), 4);
-    // Serial.print(" Y1 = ");
-    // Serial.println(myIMU.readFloatAccelY(), 4);
-    // Serial.print(" Z1 = ");
-    // Serial.println(myIMU.readFloatAccelZ(), 4);
-    // Serial.print("\nThermometer:\n");
-    // Serial.print(" Degrees C1 = ");
-    // Serial.println(myIMU.readTempC(), 4);
-    if (low_battery) {
-        if (millis() - timeSinceLastBlink > 200) {
-            if (builtinLed_Status) {
-                digitalWrite(LED_BUILTIN, LOW);
-            } else {
-                digitalWrite(LED_BUILTIN, HIGH);
-            }
-            builtinLed_Status = !builtinLed_Status;
-            timeSinceLastBlink = millis();
-        }
-    }
-    static unsigned long lastSampleTime = 0;
-    unsigned long currentTime = micros();
-    String Value;
-    if (currentTime - lastSampleTime >= SAMPLE_PERIOD) {
-        lastSampleTime += SAMPLE_PERIOD;
-        int loPlusState = digitalRead(LO_PLUS);
-        int loMinusState = digitalRead(LO_MINUS);
-        unsigned long ecgValue = analogRead(ECG_PIN);
-         if (loPlusState == HIGH || loMinusState == HIGH) {
-             Value = "Leads Off";
-             blinkLed(LED_RED,2);
-         } else {
-
-            Value = String(ecgValue);
-        }
-        Serial.println(Value);
-    }
-    characteristic->setValue(Value.c_str());
-    characteristic->notify();
-
-    delay(20);
-}
+    __SEV();
+    __WFE();
+    __WFE();
+};
